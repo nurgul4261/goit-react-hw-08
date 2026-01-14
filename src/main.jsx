036d32fa8e70ab4374fd2eaa5d72d9e28fecdee9
nurgul <nurguloksuzoglu@gmail.com>
2026-01-14 13:22:28 +0300

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter } from "react-router-dom";
-import App from "./components/App/App";
+import App from "./components/App/App.jsx";
 import { store, persistor } from "./redux/filters/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
