377ce8f55d9177f96e4c85da9bae132d9fcb2dc0
nurgul <nurguloksuzoglu@gmail.com>
2026-01-14 14:07:43 +0300

    commit

--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import RegistrationForm from "../../components/RegistrationForm/RegistrationForm";
+import styles from "./RegistrationPage.module.css";
 
 const RegistrationPage = () => {
   return (
-    <div>
+    <div className={styles.registrationPage}>
       <h2>Kayıt Ol</h2>
       <RegistrationForm />
     </div>
