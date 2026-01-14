e127147706f7b098012dc554d9a85c781f1950e9
nurgul <nurguloksuzoglu@gmail.com>
2026-01-14 13:27:21 +0300

    commit

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="home-page">
       <h1>Telefon Rehberine Hoşgeldiniz</h1>
       <p>Giriş yaparak veya kayıt olarak rehberinizi yönetebilirsiniz.</p>
     </div>
