1f123b4093b7274dcf3ab6766abcf17e185a70bb
nurgul <nurguloksuzoglu@gmail.com>
2026-01-19 14:49:59 +0300

    commit

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,7 +1,7 @@
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import styles from "./Navigation.module.css";
-import { selectIsLoggedIn } from "../redux/auth/selectors";
+import { selectIsLoggedIn } from "../../redux/auth/selectors";
 
 const Navigation = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
