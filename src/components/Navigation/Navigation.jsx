b4e0dffe37b0feb886303f903c4b7b51f77cb363
nurgul <nurguloksuzoglu@gmail.com>
2026-01-19 14:15:44 +0300

    last revision

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,7 +1,11 @@
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 import styles from "./Navigation.module.css";
+import { selectIsLoggedIn } from "../redux/auth/selectors";
 
-const Navigation = ({ isLoggedIn }) => {
+const Navigation = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
   return (
     <nav className={styles.nav}>
       <NavLink to="/" className={styles.link}>
