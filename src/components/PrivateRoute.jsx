87477976d2ab7a61d0d3810e47ee9c5759a7427e
nurgul <nurguloksuzoglu@gmail.com>
2026-01-19 14:27:44 +0300

    commit

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import { selectIsLoggedIn, selectIsRefreshing } from "../redux/auth/selectors";
